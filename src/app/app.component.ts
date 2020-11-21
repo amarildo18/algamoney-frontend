7a6c02cb1cb7214ed278e50245916eaf7a1d9a88
Amarildo Manuel Ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-21 17:04:50 +0000

    Colocando a tabela de dados responsiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'algamoney-frontend';
+  
+  lancamentos = [
+    {tipo:'DESPESA', descricao:'Compra de pão', dataVencimento:'30/06/2017', dataPagamento:null, valor:4.55, pessoa:'Padaria do Jose'},
+    {tipo:'RECEITA', descricao:'Venda de Software', dataVencimento:'10/06/2017', dataPagamento:'09/06/2017', valor:80000, pessoa:'Atacado Angola'},
+    {tipo:'DESPESA', descricao:'Impostos', dataVencimento:'20/07/2017', dataPagamento:null, valor:14315, pessoa:'Ministerio das Finanças'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {TabViewModule} from 'primeng/tabview';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
