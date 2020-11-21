d978a374d86d88894ab66485c2e5e6134830d610
Amarildo Manuel Ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-21 17:15:52 +0000

    Criando o componente de pesquisa de lançamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  lancamentos = [
-    {tipo:'DESPESA', descricao:'Compra de pão', dataVencimento:'30/06/2017', dataPagamento:null, valor:4.55, pessoa:'Padaria do Jose'},
-    {tipo:'RECEITA', descricao:'Venda de Software', dataVencimento:'10/06/2017', dataPagamento:'09/06/2017', valor:80000, pessoa:'Atacado Angola'},
-    {tipo:'DESPESA', descricao:'Impostos', dataVencimento:'20/07/2017', dataPagamento:null, valor:14315, pessoa:'Ministerio das Finanças'}
-  ];
 }
